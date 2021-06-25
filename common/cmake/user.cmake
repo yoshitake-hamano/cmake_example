357833baac6db809daecba77e3e247f529ffe643
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2021-06-26 03:24:04 +0900

    Add comment

--- common/cmake/user.cmake
+++ common/cmake/user.cmake
@@ -1,3 +1,11 @@
-
+# invoke include(user.cmake), after project()
+#
+# CMAKE_VERBOSE_MAKEFILE
+#
+# This variable is a cache entry initialized (to FALSE) by the project() command.
+# Users may enable the option in their local build tree
+# to get more verbose output from Makefile builds and
+# show each command line as it is launched.
+# https://cmake.org/cmake/help/latest/variable/CMAKE_VERBOSE_MAKEFILE.html
 set(CMAKE_VERBOSE_MAKEFILE 1)
 message(STATUS "include user.cmake")
