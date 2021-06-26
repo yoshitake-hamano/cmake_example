37766666e1e89096c9a6ea69077591b530889995
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2021-06-26 10:44:40 +0900

    Fix typo

--- common/cmake/ccache.cmake
+++ common/cmake/ccache.cmake
@@ -1,5 +1,5 @@
 
-if ("x$ENV{USER_CCACHE}" STREQUAL "x1")
+if ("x$ENV{USE_CCACHE}" STREQUAL "x1")
   message(STATUS "Enable ccache")
   set(CMAKE_C_COMPILER_LAUNCHER ccache)
   set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
