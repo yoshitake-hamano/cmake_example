56abea6deaec3b3268c4eaf041bb2c45b138dfd3
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2021-11-20 01:38:31 +0900

    Refactor

--- common/cmake/cpputest.cmake
+++ common/cmake/cpputest.cmake
@@ -7,10 +7,11 @@ else()
 endif()
 
 add_library(CppUTest STATIC IMPORTED)
-set(CppUTest_INCLUDE_DIRS $ENV{CPPUTEST_HOME}/include)
+target_include_directories(CppUTest INTERFACE $ENV{CPPUTEST_HOME}/include)
 set_property(TARGET CppUTest PROPERTY
              IMPORTED_LOCATION $ENV{CPPUTEST_HOME}/cpputest_build/src/CppUTest/libCppUTest.a)
 
 add_library(CppUTestExt STATIC IMPORTED)
+target_include_directories(CppUTestExt INTERFACE $ENV{CPPUTEST_HOME}/include)
 set_property(TARGET CppUTestExt PROPERTY
              IMPORTED_LOCATION $ENV{CPPUTEST_HOME}/cpputest_build/src/CppUTestExt/libCppUTestExt.a)
